3c266d4164905ec6d6987426cc0dcef51ab012a2
Daniel Aversa <dani.aversa@gmail.com>
2019-08-13 01:42:21 +0100

    Started js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,78 @@
-// Shuffle deck
-let deck = document.querySelector('.deck'); // Get the deck ul
-let temp = deck.cloneNode(true); // Clone list
 
-    // Shuffle cloned list
-function shuffle() {
-    for (var i = temp.children.length + 1; i--; ) {
-        temp.appendChild(temp.children[Math.random() * i |0]);
-    };
-    deck.parentNode.replaceChild(temp, deck); // Replace clone with original
+// Variables
+const cardList = [
+    "fa-github",
+    "fa-github",
+    "fa-youtube",
+    "fa-youtube",
+    "fa-codepen",
+    "fa-codepen",
+    "fa-html5",
+    "fa-html5",
+    "fa-css3",
+    "fa-css3",
+    "fa-code",
+    "fa-code",
+    "fa-book",
+    "fa-book",
+    "fa-podcast",
+    "fa-podcast"
+  ];
+let slots = document.querySelectorAll('.card-icon');
+let cards = document.querySelectorAll('.card');
+let chosenCards = [];
+let rating = 3;
+let pairsRemain = 8;
+let timeStart = false;
+let timer;
+
+/* Display Cards on page
+ *    - Shuffle Cards
+ *    - Loop through cards
+ *    - Add HTML to document
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+     return array;
 }
-shuffle();
 
-
-
-
-
-const cards = document.querySelectorAll('.card');
-
-function flipCard () {
-  this.classList.toggle('open')
+// Loop through cards and add the class to the respective element
+function dealCards() {
+    let shuffledList = shuffle(cardList);
+    for (var i = 0; i < cardList.length; i++) {
+        slots[i].classList.add(cardList[i]);
+    }
 }
-cards.forEach(card => card.addEventListener('click', flipCard))
+dealCards();
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+function flipCard() {
+    this.classList.add('open', 'show');
+    chosenCards.push(...this.firstChild.classList[2]);
+    console.log(chosenCards);
+}
+
+for(card of cards) {
+    card.addEventListener('click', flipCard);
+};
