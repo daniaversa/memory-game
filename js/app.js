a7a894459ef0330d0e662569919a51a0029f7686
Daniel Aversa <dani.aversa@gmail.com>
2019-08-20 00:59:00 +0100

    feat: keyboard shortcuts added

--- js/app.js
+++ js/app.js
@@ -5,23 +5,23 @@
 
 // Cards
 const cardList = [
-    "fa-github",
-    "fa-github",
-    "fa-youtube",
-    "fa-youtube",
-    "fa-codepen",
-    "fa-codepen",
-    "fa-html5",
-    "fa-html5",
-    "fa-css3",
-    "fa-css3",
-    "fa-code",
-    "fa-code",
-    "fa-book",
-    "fa-book",
-    "fa-podcast",
-    "fa-podcast"
-  ];
+		"fa-github",
+		"fa-github",
+		"fa-youtube",
+		"fa-youtube",
+		"fa-codepen",
+		"fa-codepen",
+		"fa-html5",
+		"fa-html5",
+		"fa-css3",
+		"fa-css3",
+		"fa-code",
+		"fa-code",
+		"fa-book",
+		"fa-book",
+		"fa-podcast",
+		"fa-podcast"
+	];
 let slots = document.querySelectorAll('.card-icon');
 let deck = document.querySelector('.deck');
 let cards = document.querySelectorAll('.card');
@@ -48,9 +48,12 @@ let starsModal = document.querySelector('.modal-stars').childNodes;
 // Restart
 let restart = document.querySelector('.restart');
 
-// Modal
-let modal = document.getElementById('modal');
+// Modals
+let modal = document.getElementById('modal-score');
+let modalKeys = document.getElementById('modal-keys');
 let modalClose = document.getElementById('modal-close');
+let modalKeysClose = document.getElementById('modal-keys-close');
+let modalKeysOpen = document.querySelector('.keys');
 let modalMoves = document.querySelector('.modal-moves');
 let modalMins = document.querySelector('.modal-mins');
 let modalSecs = document.querySelector('.modal-secs');
@@ -62,39 +65,40 @@ let modalSecs = document.querySelector('.modal-secs');
  */
 
  /**************************
-  *         Set Up         *
-  **************************
-  * - Shuffle cards        *
-  * - Loop through cards   *
-  * - Add HTML to document *
+	*         Set Up         *
+	**************************
+	* - Shuffle cards        *
+	* - Loop through cards   *
+	* - Add HTML to document *
  ***************************/
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+		var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-      }
+		while (currentIndex !== 0) {
+				randomIndex = Math.floor(Math.random() * currentIndex);
+				currentIndex -= 1;
+				temporaryValue = array[currentIndex];
+				array[currentIndex] = array[randomIndex];
+				array[randomIndex] = temporaryValue;
+			}
 
-     return array;
+		 return array;
 }
 
 // Loop through cards and add the class to the respective element
 function dealCards() {
-    let shuffledList = shuffle(cardList);
-    for (var i = 0; i < cardList.length; i++) {
-        // Remove old cards when restarting
-        slots[i].className = '';
-        cards[i].classList.remove("open", "show", "match");
-        // Add cards to the table
-        slots[i].classList.add('fa', cardList[i]);
-    }
+		let shuffledList = shuffle(cardList);
+		for (var i = 0; i < cardList.length; i++) {
+				// Remove old cards when restarting
+				slots[i].className = '';
+				cards[i].classList.remove("open", "show", "match");
+				// Add cards to the table
+				slots[i].classList.add('fa', cardList[i]);
+		}
 }
+
 dealCards();
 
 /**************************
@@ -106,162 +110,223 @@ deck.addEventListener('click', flipCard);
 
 // Flips a card and calls showCard and addChosenCard function
 function flipCard(e) {
-    let chosen = e.target;
-    // Checks if clicked element is a card, is not open, and if there are already 2 open cards
-    if (chosen.nodeName === "LI" && !chosen.classList.contains("open", "show", "match") && chosenCards.length !== 2) {
-        chosen.classList.add('open');
-        addChosenCard(chosen);
-        if (timer === false && pairsRemain > 0) {
-            timerOn();
-        }
-        setTimeout(showCard, 300, chosen);
-    }
+		let chosen = e.target;
+		// Checks if clicked element is a card, is not open, and if there are already 2 open cards
+		if (chosen.nodeName === "LI" && !chosen.classList.contains("open", "show", "match") && chosenCards.length !== 2) {
+				chosen.classList.add('open');
+				addChosenCard(chosen);
+				if (timer === false && pairsRemain > 0) {
+						timerOn();
+				}
+				setTimeout(showCard, 300, chosen);
+		}
 }
+
 // Reveals the card
 function showCard(card) {
-    card.classList.add('show')
+		card.classList.add('show')
 }
 
 // Add clicked cards to check array and calls checkMatch function if 2 cards are clicked.
 function addChosenCard(card){
-    chosenCards.push(card);
-    if (chosenCards.length === 2) {
-        checkMatch();
-        moveCount();
-    }
+		chosenCards.push(card);
+		if (chosenCards.length === 2) {
+				checkMatch();
+				moveCount();
+		}
 }
 
 // Checks cards for matches
 function checkMatch() {
-    // Matching cards case
-    if (chosenCards[0].innerHTML === chosenCards[1].innerHTML) {
-        match();
-    } else { // No match case
-        setTimeout(noMatch, 800);
-    }
+		// Matching cards case
+		if (chosenCards[0].innerHTML === chosenCards[1].innerHTML) {
+				match();
+		} else { // No match case
+				setTimeout(noMatch, 800);
+		}
 }
 
 // Cards Match: Lock cards open and call end game condition function
 function match() {
-    for(card of chosenCards) {
-        card.classList.add("match");
-    };
-    pairsRemain--;
-    checkWin();
-    empty();
+		for(card of chosenCards) {
+				card.classList.add("match");
+		};
+		pairsRemain--;
+		checkWin();
+		empty();
 }
 
 // Cards don't match: Flips cards down
 function noMatch() {
-    for (card of chosenCards) {
-        card.classList.remove("open", "show");
-    };
-    empty();
+		for (card of chosenCards) {
+				card.classList.remove("open", "show");
+		};
+		empty();
 }
 
 // Empty the array after checks
 function empty() {
-    chosenCards = [];
+		chosenCards = [];
 }
 
 // Checks for Win condition
 function checkWin() {
-    if (pairsRemain === 0) {
-        timerOff();
-        openModal();
-    };
+		if (pairsRemain === 0) {
+				timerOff();
+				openModal();
+		};
 }
 
 // Increment move counter
 function moveCount() {
-    moves++;
-    counter.innerHTML = moves;
-    checkRating();
+		moves++;
+		counter.innerHTML = moves;
+		checkRating();
 }
 
 // Stars rating
 function checkRating() {
-  if (moves === 12) {
-      // Remove third star
-      stars[5].classList.add('lost')
-      starsModal[5].classList.add('lost')
-  } else if (moves === 20) {
-      // Remove second star
-      stars[3].classList.add('lost')
-      starsModal[3].classList.add('lost')
-  }
+		if (moves === 12) {
+				// Remove third star
+				stars[5].classList.add('lost')
+				starsModal[5].classList.add('lost')
+		} else if (moves === 20) {
+				// Remove second star
+				stars[3].classList.add('lost')
+				starsModal[3].classList.add('lost')
+		}
 }
 
 // Timer
 function timerOn() {
-    if (timer === false) {
-        count = setInterval(countTime, 1000);
-        timer = true;
-    }
+		if (timer === false) {
+				count = setInterval(countTime, 1000);
+				timer = true;
+		}
 }
 
 function timerOff() {
-        clearInterval(count);
-        count = 0;
-        timer = false;
+		clearInterval(count);
+		count = 0;
+		timer = false;
 }
 
 function countTime() {
-    ++totalTime;
-    secs.innerHTML = pad(totalTime%60);
-    mins.innerHTML = pad(parseInt(totalTime/60));
+		++totalTime;
+		secs.innerHTML = pad(totalTime%60);
+		mins.innerHTML = pad(parseInt(totalTime/60));
 }
 
 function pad(val) {
-    let valString = val + "";
-    if (valString.length < 2) {
-        return "0" + valString;
-    } else {
-        return valString;
-    }
+		let valString = val + "";
+		if (valString.length < 2) {
+				return "0" + valString;
+		} else {
+				return valString;
+		}
 }
 
 // Restart game
 restart.addEventListener('click', restartGame);
 
 function restartGame() {
-    // Restart moves,totalTime and pairsRemain counter
-    moves = -1;
-    moveCount();
-    pairsRemain = 8;
-    totalTime = 0;
-    // Restart stars rating
-    stars[3].classList.remove('lost');
-    starsModal[3].classList.remove('lost');
-    stars[5].classList.remove('lost');
-    starsModal[5].classList.remove('lost');
-    dealCards();
-    // Restart timer
-    secs.innerHTML = "00";
-    mins.innerHTML = "00";
-    if (timer === true) {
-        timerOff();
-    }
+		// Restart moves,totalTime and pairsRemain counter
+		moves = -1;
+		moveCount();
+		pairsRemain = 8;
+		totalTime = 0;
+		// Restart stars rating
+		stars[3].classList.remove('lost');
+		starsModal[3].classList.remove('lost');
+		stars[5].classList.remove('lost');
+		starsModal[5].classList.remove('lost');
+		dealCards();
+		// Restart timer
+		secs.innerHTML = "00";
+		mins.innerHTML = "00";
+		if (timer === true) {
+				timerOff();
+		}
 }
 
 // Update and open modal
 function openModal () {
-    modalMoves.innerHTML = moves;
-    modalSecs.innerHTML = pad(totalTime%60);
-    modalMins.innerHTML = pad(parseInt(totalTime/60));
-    modal.style.display = "block";
+		modalMoves.innerHTML = moves;
+		modalSecs.innerHTML = pad(totalTime%60);
+		modalMins.innerHTML = pad(parseInt(totalTime/60));
+		modal.style.display = "block";
 }
 
 
 // Close modal Button and restart game
 modalClose.onclick = function() {
-    restartGame();
-    modal.style.display = "none";
+		restartGame();
+		modal.style.display = "none";
+}
+
+// Open Keys Modals
+modalKeysOpen.onclick = function() {
+		modalKeys.style.display = "block";
+}
+
+// Close Keys modal
+modalKeysClose.onclick = function() {
+		modalKeys.style.display = "none";
 }
 
 // Close modal if anywhere outside of it is clicked
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+		if (event.target == modal) {
+			modal.style.display = "none";
+		} else if (event.target == modalKeys) {
+			modalKeys.style.display = "none";
+		}
+}
+
+// Keyboard shortcuts
+document.onkeyup = function(e) {
+		if (e.which == 49) {        // 1
+				flipCardKeyboard(0);
+		} else if (e.which == 50) { // 2
+				flipCardKeyboard(1);
+		} else if (e.which == 51) { // 3
+				flipCardKeyboard(2);
+		} else if (e.which == 52) { // 4
+				flipCardKeyboard(3);
+		} else if (e.which == 81) { // Q
+				flipCardKeyboard(4);
+		} else if (e.which == 87) { // W
+				flipCardKeyboard(5);
+		} else if (e.which == 69) { // E
+				flipCardKeyboard(6);
+		} else if (e.which == 82) { // R
+				flipCardKeyboard(7);
+		} else if (e.which == 65) { // A
+				flipCardKeyboard(8);
+		} else if (e.which == 83) { // S
+				flipCardKeyboard(9);
+		} else if (e.which == 68) { // D
+				flipCardKeyboard(10);
+		} else if (e.which == 70) { // F
+				flipCardKeyboard(11);
+		} else if (e.which == 90) { // Z
+				flipCardKeyboard(12);
+		} else if (e.which == 88) { // X
+				flipCardKeyboard(13);
+		} else if (e.which == 67) { // C
+				flipCardKeyboard(14);
+		} else if (e.which == 86) { // V
+				flipCardKeyboard(15);
+		}
+}
+
+function flipCardKeyboard(card) {
+		if (!cards[card].classList.contains("open", "show", "match") && chosenCards.length !== 2) {
+				cards[card].classList.add('open');
+				addChosenCard(cards[card]);
+				if (timer === false && pairsRemain > 0) {
+						timerOn();
+				}
+				setTimeout(showCard, 300, cards[card]);
+		}
 }
